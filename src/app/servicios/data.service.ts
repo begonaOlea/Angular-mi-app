9c1cfe886845528a4e41d628d580c813a98d6e67
begonaOlea <bolea@programia.es>
2021-09-15 12:33:09 +0200

    operaciones get/put/post/delete HTTP

--- src/app/servicios/data.service.ts
+++ src/app/servicios/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ProductosBaseDatos } from '../entidades/productosbasedatos';
 
@@ -15,4 +15,37 @@ export class DataService {
   public getProductos(): Observable<ProductosBaseDatos[]>{
        return this.http.get<ProductosBaseDatos[]>(this.urlBase);
   }
+
+  public getProducto(id : number): Observable<ProductosBaseDatos>{
+     const url = `${this.urlBase}/${id}`;
+     return this.http.get<ProductosBaseDatos>(url);
+  }
+
+   //POST
+  public addProducto(producto: ProductosBaseDatos): Observable<ProductosBaseDatos> {
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    };
+    const obs = this.http.post<ProductosBaseDatos>(this.urlBase, producto, httpOptions);
+    //obs.subscribe((nueva) => this.altaEvent.emit(nueva.id));
+    return obs;
+  }
+
+  //UPDATE
+  public updateProducto(producto: ProductosBaseDatos): Observable<ProductosBaseDatos>{
+     const url = `${this.urlBase}/${producto.idProducto}`;
+     const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+     };
+    const obs = this.http.put<ProductosBaseDatos>(url, producto, httpOptions);
+    return obs;
+  }
+
+  //DELETE
+  public deleteProducto(id: number): Observable<string>{
+    const url = `${this.urlBase}/${id}`;
+    const obs = this.http.delete<string>(url);
+    return obs;
+  }
+
 }
